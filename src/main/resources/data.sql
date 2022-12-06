02c152e7c1d77626cd883422fd04a275bbd1fd20
flino1934 <f.lino1934@hotmail.com>
2022-12-06 19:03:28 -0300

    Criando massa de dados para client
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Felipe Lino','49366901808', 7500.0, TIMESTAMP WITH TIME ZONE '1997-06-02T20:50:07.12345Z',0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Sarah Lino','49366901808', 0.0, TIMESTAMP WITH TIME ZONE '2005-05-02T20:50:07.12345Z',0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Leonardo Requena','49366901808', 3200.0, TIMESTAMP WITH TIME ZONE '1998-02-02T20:50:07.12345Z',1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Lucas Gomes','49366901808', 100.0, TIMESTAMP WITH TIME ZONE '1999-04-02T20:50:07.12345Z',0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Julio Cesar','49366901808', 3500.0, TIMESTAMP WITH TIME ZONE '2000-03-02T20:50:07.12345Z',0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Mika','49366901808', 500.0, TIMESTAMP WITH TIME ZONE '2005-06-02T20:50:07.12345Z',0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Mayara Ferreira','49366901808', 7500.0, TIMESTAMP WITH TIME ZONE '1995-06-02T20:50:07.12345Z',0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Maytê Khezam','49366901808', 3000.0, TIMESTAMP WITH TIME ZONE '2000-06-02T20:50:07.12345Z',0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Nicole Requena','49366901808', 3000.0, TIMESTAMP WITH TIME ZONE '2000-06-02T20:50:07.12345Z',0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Dominique Requena','49366901808', 3000.0, TIMESTAMP WITH TIME ZONE '1999-06-02T20:50:07.12345Z',1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Dominique Requena','49366901808', 3000.0, TIMESTAMP WITH TIME ZONE '1999-06-02T20:50:07.12345Z',1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Sarah Lino','49366901808', 0.0, TIMESTAMP WITH TIME ZONE '2005-05-02T20:50:07.12345Z',0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Leonardo Requena','49366901808', 3200.0, TIMESTAMP WITH TIME ZONE '1998-02-02T20:50:07.12345Z',1);
